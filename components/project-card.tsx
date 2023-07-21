6ccbf35e5fb8db6c20e342e4eb77b554a6c377e6
Rokas <rookas.rudzenskas@gmail.com>
2023-07-21 17:21:50 +0300

    FIX

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -48,21 +48,21 @@ const ProjectCard: NextPage<ProjectCardType> = ({
       style={card1Style}
     >
       <img
-        className="self-stretch relative rounded-3xs max-w-full overflow-hidden h-[200px] shrink-0 object-cover"
+        className="relative rounded-3xs max-w-full overflow-hidden h-[200px] shrink-0 object-cover"
         alt=""
         src={propertyImage}
       />
-      <div className="self-stretch flex flex-row p-2.5 items-start justify-start">
+      <div className="flex flex-row p-2.5 items-start justify-start">
         <div className="flex-1 relative leading-[20.32px] font-medium">
           92 ALLIUM PLACE, ORLANDO FL 32827
         </div>
       </div>
-      <div className="self-stretch flex flex-row p-2.5 items-start justify-start text-[13.55px] text-primary-500">
+      <div className="flex flex-row p-2.5 items-start justify-start text-[13.55px] text-primary-500">
         <div className="relative leading-[18.98px] font-semibold">
           $ 590,693
         </div>
       </div>
-      <div className="self-stretch flex flex-row py-0 px-2.5 items-start justify-start gap-[17px] text-[10.84px] text-gray-500">
+      <div className="flex flex-row py-0 px-2.5 items-start justify-start gap-[17px] text-[10.84px] text-gray-500">
         <div className="flex flex-row items-center justify-start gap-[4.35px]">
           <img className="relative w-[17.42px] h-[17.42px]" alt="" src={car} />
           <div className="relative leading-[16.27px] font-medium">4</div>
@@ -86,7 +86,7 @@ const ProjectCard: NextPage<ProjectCardType> = ({
           </div>
         </div>
       </div>
-      <div className="self-stretch flex flex-row items-center justify-between text-center text-[11.61px]">
+      <div className="flex flex-row items-center justify-between text-center text-[11.61px]">
         <div className="flex flex-row items-center justify-start gap-[5.81px]">
           <img
             className="relative rounded-[50%] w-[27.57px] h-[27.57px] object-cover"
